8fc469f137b608dc55c94410d280c8768114782a
Asad Sheikh <asadqadri239@gmail.com>
2026-08-03 03:18:23 +0500

    Add base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ecommerce-web/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
